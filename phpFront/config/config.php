fbf4289bc5cc7b8b1f3d3a5852d285ce25993717
xubeiyan <yueyongdong@gmail.com>
2016-08-29 18:19:03 +0800

    add the function that read database by using API of back end...

--- phpFront/config/config.php
+++ phpFront/config/config.php
@@ -3,7 +3,7 @@
 * 设置文件
 */
 $config = Array(
-	'backURI' => '../phpBack/index.php', // 后台地址
+	'backURI' => 'http://localhost/kagariNMB/phpBack/', // 后台地址
 	'responseType' => 'json'
 );
 
--- phpFront/kagari/controller.php
+++ phpFront/kagari/controller.php
@@ -6,7 +6,30 @@ class Controller {
 	// 数据库值替换
 	public static function dbDataReplace($list, $toReplace) {
 		global $config;
-		
+		foreach ($list as $key => $value) {
+			//print_r($key . '|' . $value);
+			if ($key == 'areaLists') {
+				$opts = Array(
+					'http' => Array(
+						'method' => 'GET',
+						'user_agent' => 'KagariNMBFront'
+					)
+				);
+				$context = stream_context_create($opts);
+				$json = file_get_contents($config['backURI'] . $value, false, $context);
+				$array = json_decode($json, TRUE);
+				//print_r($array);
+				if (isset($array['response']['areas'])) {
+					//print_r($array);
+					$string = '';
+					foreach ($array['response']['areas'] as $arrkey => $arrval) {
+						$string .= $arrval['area_name'] . '<br />';
+					}
+					$toReplace = str_replace('%' . $key . '%', $string , $toReplace);
+				}
+				
+			}
+		}
 		return $toReplace;
 	}
 	
