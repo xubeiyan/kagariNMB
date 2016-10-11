c81fd8b673dd59ac1d8f32b44c2ad3e61f1b00fd
xubeiyan <yueyongdong@gmail.com>
2016-10-11 15:21:03 +0800

    add the switch of area lists...

---
+++ phpFront/js/angular.js
@@ -0,0 +1,19 @@
+/**
+* kagari是某个库
+*/
+
+var kagari = function () {
+	var that = {
+		switchListPanel: function () {
+			var area = document.getElementById("areas");
+			console.log('!');
+			if (area.style.display == 'none') {
+				area.style.display = 'block';
+			} else {
+				area.style.display = 'none';
+			}
+		}
+	};
+	return that;
+	},
+	k = kagari();
