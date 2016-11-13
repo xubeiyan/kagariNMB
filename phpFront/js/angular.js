4209e15d3211500667b3bc26529651613b39c55d
yyd <yueyongdong@gmail.com>
2016-11-13 23:05:01 +0800

    add functions...

--- phpFront/js/angular.js
+++ phpFront/js/angular.js
@@ -1,15 +1,26 @@
 /**
-* kagari是某个库
+* angular是某个库，反正和Angular.js没太大关系
 */
 
 var kagari = function () {
 	var that = {
 		switchListPanel: function () {
 			var area = document.getElementById("areas");
-			console.log('!');
 			if (area.style.display == '' || area.style.display == 'none') {
+				console.log('areas list show~');
 				area.style.display = 'block';
 			} else {
+				console.log('areas list hide~');
+				area.style.display = 'none';
+			}
+		},
+		switchFunctionPanel: function () {
+			var area = document.getElementById("functions");
+			if (area.style.display == '' || area.style.display == 'none') {
+				console.log('functions list show~');
+				area.style.display = 'block';
+			} else {
+				console.log('functions list hide~');
 				area.style.display = 'none';
 			}
 		}
