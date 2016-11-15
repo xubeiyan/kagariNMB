0393be90c19e27a6b98b43ee58c507c254861d6e
xubeiyan <yueyongdong@gmail.com>
2016-11-15 13:49:46 +0800

    add the URI of images in config.php...

--- phpFront/config/config.php
+++ phpFront/config/config.php
@@ -4,6 +4,7 @@
 */
 $config = Array(
 	'backURI' => 'http://localhost/kagariNMB/phpBack/', // 后台地址
+	'imgURI' => 'http://localhost/kagariNMB/phpBack/images/', // 图片地址
 	'responseType' => 'json',
 	'userAgent' => 'KagariNMBFront' // 客户端指定串
 );
